69e510e38c5b3f6dbc8a8d2187174f6a3aa0923b
James Acres <jamesacres@gmail.com>
2024-07-06 13:48:48 +0100

    change to unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
